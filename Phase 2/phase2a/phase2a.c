6e3f43b54cfa1bde40e1bc318ac500b90c9f1892
Kyle Snowden <39664771+katteranga@users.noreply.github.com>
2019-10-13 19:51:47 -0700

    Implemented Stubs & syscall handlers.

--- Phase 2/phase2a/phase2a.c
+++ Phase 2/phase2a/phase2a.c
@@ -12,7 +12,15 @@
 #define TAG_KERNEL 0
 #define TAG_USER 1
 
+void (*USLOSS_SyscallVec[USLOSS_MAX_SYSCALLS])(int dev, void *arg) = { NULL };
+
+
 static void SpawnStub(USLOSS_Sysargs *sysargs);
+static void WaitStub(USLOSS_Sysargs *sysargs);
+static void TerminateStub(USLOSS_Sysargs *sysargs);
+static void GetProcInfoStub(USLOSS_Sysargs *sysargs);
+static void GetPid (USLOSS_Sysargs *sysargs);
+static void GetTimeOfDay (USLOSS_Sysargs *sysargs);
 
 /*
  * IllegalHandler
@@ -22,7 +30,7 @@ static void SpawnStub(USLOSS_Sysargs *sysargs);
  */
 
 static void 
-IllegalHandler(int type, void *arg) 
+IllegalHandler(int type, void *arg)
 {
     P1_ProcInfo info;
     assert(type == USLOSS_ILLEGAL_INT);
@@ -45,8 +53,33 @@ IllegalHandler(int type, void *arg)
  */
 
 static void 
-SyscallHandler(int type, void *arg) 
+SyscallHandler(int type, void *arg)
+//SyscallHandler(int type, USLOSS_Sysargs *arg)
 {
+//    if ((USLOSS_PsrGet() & USLOSS_PSR_CURRENT_MODE) != USLOSS_PSR_CURRENT_MODE) {
+//
+//
+//        // JACK IS THIS HOW THIS WORKS? I DON'T KNOW
+//        int pid = P1_GetPid();
+//        P1_ProcInfo currentInfo;
+//        int rc = P1_GetProcInfo(pid, &currentInfo);
+//        assert(rc == P1_SUCCESS);
+//        if (currentInfo.tag == TAG_KERNEL) {
+//            USLOSS_Console("ERROR: Call to SyscallHandler from user mode in kernel-level process.\n");
+//            USLOSS_IllegalInstruction();
+//            P1_Quit(1024);
+//        } else {
+//            USLOSS_Console("ERROR: Call to SyscallHandler from user mode in user-level process.\n");
+//            P2_Terminate(2048);
+//        }
+//    }
+
+
+    if (USLOSS_SyscallVec[((USLOSS_Sysargs*) arg)->number] == NULL) {
+        ((USLOSS_Sysargs*) arg)->arg4 = (void*) P2_INVALID_SYSCALL;
+    } else {
+        USLOSS_SyscallVec[((USLOSS_Sysargs*) arg)->number](0, arg);
+    }
 
 }
 
@@ -69,7 +102,25 @@ P2ProcInit(void)
     // call P2_SetSyscallHandler to set handlers for all system calls
     rc = P2_SetSyscallHandler(SYS_SPAWN, SpawnStub);
     assert(rc == P1_SUCCESS);
+
+    rc = P2_SetSyscallHandler(SYS_WAIT, WaitStub);
+    assert(rc == P1_SUCCESS);
+
+    rc = P2_SetSyscallHandler(SYS_TERMINATE, TerminateStub);
+    assert(rc == P1_SUCCESS);
+
+    rc = P2_SetSyscallHandler(SYS_GETPROCINFO, GetProcInfoStub);
+    assert(rc == P1_SUCCESS);
+
+    rc = P2_SetSyscallHandler(SYS_GETPID, GetPid);
+    assert(rc == P1_SUCCESS);
+
+    rc = P2_SetSyscallHandler(SYS_GETTIMEOFDAY, GetTimeOfDay);
+    assert(rc == P1_SUCCESS);
+
+
 }
+
 
 /*
  * P2_SetSyscallHandler
@@ -81,6 +132,12 @@ P2ProcInit(void)
 int
 P2_SetSyscallHandler(unsigned int number, void (*handler)(USLOSS_Sysargs *args))
 {
+    if (number < 0 || number >= USLOSS_MAX_SYSCALLS) {
+        return P2_INVALID_SYSCALL;
+    }
+
+    // JACK I'M ALSO CONFUSED HERE
+    USLOSS_SyscallVec[number] = handler;
 
     return P1_SUCCESS;
 }
@@ -118,6 +175,29 @@ P2_Spawn(char *name, int(*func)(void *arg), void *arg, int stackSize, int priori
 }
 
 /*
+ * SpawnStub
+ *
+ * Stub for Sys_Spawn system call.
+ *
+ */
+
+static void
+SpawnStub(USLOSS_Sysargs *sysargs)
+{
+    int (*func)(void *) = sysargs->arg1;
+    void *arg = sysargs->arg2;
+    int stackSize = (int) sysargs->arg3;
+    int priority = (int) sysargs->arg4;
+    char *name = sysargs->arg5;
+    int pid;
+    int rc = P2_Spawn(name, func, arg, stackSize, priority, &pid);
+    if (rc == P1_SUCCESS) {
+        sysargs->arg1 = (void *) pid;
+    }
+    sysargs->arg4 = (void *) rc;
+}
+
+/*
  * P2_Wait
  *
  * Wait for a user-level process.
@@ -128,6 +208,23 @@ int
 P2_Wait(int *pid, int *status) 
 {
     return P1_SUCCESS;
+}
+
+/*
+ * WaitStub
+ *
+ * Stub for Sys_Wait system call.
+ *
+ */
+
+static void
+WaitStub(USLOSS_Sysargs *sysargs)
+{
+    int pid = (int) sysargs->arg1;
+    int status;
+
+    int rc = P2_Wait(pid, &status);
+    sysargs->arg4 = (void *) rc;
 }
 
 /*
@@ -144,26 +241,76 @@ P2_Terminate(int status)
 }
 
 /*
- * SpawnStub
+ * TerminateStub
  *
- * Stub for Sys_Spawn system call. 
+ * Stub for Sys_Terminate system call.
  *
  */
 
-static void 
-SpawnStub(USLOSS_Sysargs *sysargs) 
+static void
+TerminateStub(USLOSS_Sysargs *sysargs)
 {
-    int (*func)(void *) = sysargs->arg1;
-    void *arg = sysargs->arg2;
-    int stackSize = (int) sysargs->arg3;
-    int priority = (int) sysargs->arg4;
-    char *name = sysargs->arg5;
-    int pid;
-    int rc = P2_Spawn(name, func, arg, stackSize, priority, &pid);
-    if (rc == P1_SUCCESS) {
-        sysargs->arg1 = (void *) pid;
-    }
+    int terminationCode = (int) sysargs->arg1;
+    P2_Terminate(terminationCode);
+}
+
+/*
+ * GetProcInfo
+ *
+ * Returns process information for a given pid.
+ *
+ */
+
+int
+GetProcInfo(int pid, P1_ProcInfo *info) {
+    return P1_GetProcInfo(pid, info);
+}
+
+/*
+ * GetProcInfoStub
+ *
+ * Stub for Sys_ProcInfo system call.
+ *
+ */
+
+static void
+GetProcInfoStub(USLOSS_Sysargs *sysargs) {
+    int pid = (int) sysargs->arg1;
+    P1_ProcInfo *info = sysargs->arg2;
+
+    int rc = GetProcInfo(pid, info);
+
     sysargs->arg4 = (void *) rc;
+}
+
+/*
+ * GetPid
+ *
+ * Returns the current pid.
+ *
+ */
+
+static void
+GetPid (USLOSS_Sysargs *sysargs)
+{
+    sysargs->arg1 = (void *) P1_GetPid();
+}
+
+/*
+ * GetTimeOfDay
+ *
+ * Returns the current USLOSS time.
+ *
+ */
+
+static void
+GetTimeOfDay (USLOSS_Sysargs *sysargs)
+{
+    int status;
+    int rc = USLOSS_DeviceInput(USLOSS_CLOCK_DEV, 0, &status);
+    assert(rc = USLOSS_DEV_OK);
+
+    sysargs->arg1 = (void *) status;
 }
 
 
