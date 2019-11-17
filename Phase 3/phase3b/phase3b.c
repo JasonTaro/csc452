35371811b6c0902eaeb378f1c0a7da4ef621e5a1
Kyle Snowden <39664771+katteranga@users.noreply.github.com>
2019-11-17 13:28:01 -0700

    Added code to initialize empty page table entries, then fill them in on interrupt

--- Phase 3/phase3b/phase3b.c
+++ Phase 3/phase3b/phase3b.c
@@ -15,6 +15,11 @@
 void
 P3PageFaultHandler(int type, void *arg)
 {
+    if ((USLOSS_PsrGet() & USLOSS_PSR_CURRENT_MODE) != USLOSS_PSR_CURRENT_MODE) {
+        USLOSS_Console("ERROR: Call to P3PageFaultHandler from user mode.\n");
+        USLOSS_IllegalInstruction();
+    }
+
     /*******************
 
     if the cause is USLOSS_MMU_FAULT (USLOSS_MmuGetCause)
@@ -31,12 +36,47 @@ P3PageFaultHandler(int type, void *arg)
         USLOSS_Halt(1)
     *********************/
 
+    int rc;
+    if (USLOSS_MmuGetCause() == USLOSS_MMU_FAULT) {
+        USLOSS_PTE* currentPageTable;
+        if (P3PageTableGet(P1_GetPid(), &currentPageTable) != P1_SUCCESS) {
+            USLOSS_Console("ERROR: Current process does not have page table\n");
+            USLOSS_Halt(1);
+        } else {
+            int offset = (int) arg;
+            int page = offset / USLOSS_MmuPageSize();
+            USLOSS_Console("%d\n", page);
+
+            currentPageTable[page].incore = 1;
+            currentPageTable[page].frame = page;
+            currentPageTable[page].read = 1;
+            currentPageTable[page].write = 1;
+
+            rc = USLOSS_MmuSetPageTable(currentPageTable); assert(rc == USLOSS_MMU_OK);
+        }
+    } else {
+        USLOSS_Console("ERROR: MMU Interrupt caused by invalid protections\n");
+        USLOSS_Halt(1);
+    }
 }
 
 USLOSS_PTE *
 P3PageTableAllocateEmpty(int pages)
 {
-    USLOSS_PTE  *table = NULL;
-    // allocate and initialize an empty page table
+    if ((USLOSS_PsrGet() & USLOSS_PSR_CURRENT_MODE) != USLOSS_PSR_CURRENT_MODE) {
+        USLOSS_Console("ERROR: Call to P3PageTableAllocateEmpty from user mode.\n");
+        USLOSS_IllegalInstruction();
+    }
+
+    if (pages < 0) { return NULL; }
+
+    USLOSS_PTE  *table = malloc(sizeof(USLOSS_PTE) * pages);
+    if(table == NULL){ return NULL; }
+
+    for(int i = 0; i < pages; i++){
+        table[i].incore = 0;
+    }
     return table;
+
 }
+
